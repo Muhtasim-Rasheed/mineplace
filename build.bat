34edefc1feadad8f77f7ccb822918191068e5af9
Muhtasim-Rasheed <build.stonehenge@gmail.com>
2024-12-14 18:15:40 +0600

    Shit windows freaks out abt REM

--- build.bat
+++ build.bat
@@ -1,8 +1,3 @@
-REM PyInstaller build script (name: mineplace)
-python -m pyinstaller --onefile --add-data "assets;assets" main.py --name mineplace.exe --noconsole REM For Windows
+python -m pyinstaller --onefile --add-data "assets;assets" main.py --name mineplace.exe --noconsole
 
-REM Remove useless files
-REM rm -rf build mineplace.exe.spec
-REM Do it the Windows way
 rmdir /s /q build
-del mineplace.exe.spec
