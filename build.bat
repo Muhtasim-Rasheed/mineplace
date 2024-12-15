2ce247d6fb4cce1c22afa3b1a9fe8feb22ac6d5c
Muhtasim-Rasheed <build.stonehenge@gmail.com>
2024-12-15 23:26:11 +0600

    BUILDING PYINSTALLER APPS ON BINBOWS IS SO HARD

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
-python -m pyinstaller --onefile --add-data "assets;assets" main.py --name mineplace.exe --noconsole
-
-rmdir /s /q build
+pyinstaller --onefile --add-data "assets;assets" main.py --name mineplace.exe --noconsole
